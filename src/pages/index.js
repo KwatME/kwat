2582a97730267f7779b36dc04e358c43d4ad68a8
kwatme <kwat.me@icloud.com>
2020-12-17 02:05:45 -0800

    bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import "react-bulma-components/dist/react-bulma-components.min.css"
+import "./index.scss"
+
 function Index() {
   return (
     <div>
       <Layout>
         <h1>This is Index</h1>
       </Layout>
+      <div className="buttons">
+        <a className="button is-primary">Primary</a>
+      </div>
     </div>
   )
 }
