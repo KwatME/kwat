21651b9ea0f386bb07b30327a71cf77dd2f6833f
kwatme <kwat.me@icloud.com>
2020-12-18 15:22:01 -0800

    Helmet in SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -61,7 +61,24 @@ function SEO({ title, description = "", meta = [] }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
+      />
+
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+        crossOrigin="anonymous"
+      />
+
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css"
+      />
+    </Helmet>
   )
 }
 
--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -1,30 +1,10 @@
 import React from "react"
-import { Helmet } from "react-helmet"
 
 import SocialIcon from "./social-icon"
 
 function SocialIcons({ className }) {
   return (
     <>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
-        />
-
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css"
-        />
-      </Helmet>
-
       <SocialIcon
         name="GitHub"
         url="https://github.com/KwatME"
